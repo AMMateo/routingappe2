fcb59384f9e9e03bb3a552ac60ffa23c075dec8d
AMMateo <matty200628@gmail.com>
2023-12-01 14:00:49 -0600

    Navegacion con tabs
    Uso de LoadChildren para instalar las paginas
    Modificación de tabs para navegar

---
+++ src/app/tabs/medium/medium.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medium',
+  templateUrl: './medium.page.html',
+  styleUrls: ['./medium.page.scss'],
+})
+export class MediumPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
